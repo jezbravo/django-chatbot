2b213ce47fc7070b622661d863c871a2ac111507
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-03-15 21:16:48 -0300

    feat: :construction: Add templates

--- django_chatbot/settings.py
+++ django_chatbot/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'django_chatbot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
