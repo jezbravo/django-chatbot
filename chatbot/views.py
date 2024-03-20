f3cb73433121c8f12f47a5e4f6d1fceb887393b8
Ezequiel Bravo <126541104+jezbravo@users.noreply.github.com>
2024-03-19 22:16:28 -0300

    Update files

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,7 +1,9 @@
 import os
 import environ
 import google.generativeai as genai
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 
 env = environ.Env()
@@ -86,10 +88,48 @@ def ask_genai(message):
 
 # print(convo.last.text)
 
-# Create your views here.
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
         response_text = ask_genai(message)
         return JsonResponse({'message': message, 'response': response_text})
     return render(request, 'chatbot.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatbot')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(request, user)
+                return redirect('chatbot')
+            except:
+                error_message = 'Error creating account'
+                return render(request, 'register.html', {'error_message': error_message})
+        else:
+            error_message = "The password don't match"
+            return render(request, 'register.html', {'error_message': error_message})
+    return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
